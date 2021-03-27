da8cdcb91922665583d85f5c91cbae41cd016b94
Siddharth Gupta <81249728+Infinite-Blue-1042@users.noreply.github.com>
2021-03-27 14:23:37 -0700

    Create Run.bash

---
+++ Run.bash
@@ -0,0 +1,21 @@
+set -e
+if [ ! -f ../bin/go ]; then
+	echo 'run.bash must be run from $GOROOT/src after installing cmd/go' 1>&2
+	exit 1
+fi
+eval $(../bin/go env)
+export GOROOT
+export GOPATH=/nonexist-gopath
+unset CDPATH
+export GOBIN=$GOROOT/bin
+unset GOFLAGS
+unset GO111MODULE
+export GOHOSTOS
+export CC
+ulimit -c 0
+[ "$(ulimit -H -n)" = "unlimited" ] || ulimit -S -n $(ulimit -H -n)
+[ "$(ulimit -H -d)" = "unlimited" ] || ulimit -S -d $(ulimit -H -d)
+if ulimit -T &> /dev/null; then
+	[ "$(ulimit -H -T)" = "unlimited" ] || ulimit -S -T $(ulimit -H -T)
+fi
+exec ../bin/go tool dist test -rebuild "$@"
